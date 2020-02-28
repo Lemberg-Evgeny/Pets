228c2559b71526fd7a565089fb7a7c988fc8bb49
LEMZEN <LEMZEN@hotmail.com>
2020-02-28 12:11:09 +0200

    Gallery fancy

---
+++ gallery/scripts/data-fancybox-Gallery.js
@@ -0,0 +1,12 @@
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+        "slideShow",
+        "thumbs",
+        "close"
+    ],
+    protect: true,
+    loop: true,
+    arrows: true,
+    infobar: true,
+    transitionEffect: "circular"
+});
